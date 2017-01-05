62f4508071d53bf9ccd8de226e11b0d7599c9618
Justin C. Bagley <centrarchidae@gmail.com>
2017-01-05 01:33:38 -0600

    Added two example run folders to repo
    Added “Ex1_GaussClust” and “Ex2_bgmmSensTest” folders, including
    example run files and results.

---
+++ Ex2_bgmmSensTest/logicalTests.r
@@ -0,0 +1,18 @@
+
+#!/usr/bin/env Rscript
+
+identical(tijObjList[1][[1]][1:31,],tijObjList[2][[1]][1:31,])
+identical(tijObjList[1][[1]][1:31,],tijObjList[3][[1]][1:31,])
+identical(tijObjList[1][[1]][1:31,],tijObjList[4][[1]][1:31,])
+identical(tijObjList[1][[1]][1:31,],tijObjList[5][[1]][1:31,])
+identical(tijObjList[1][[1]][1:31,],tijObjList[6][[1]][1:31,])
+identical(tijObjList[2][[1]][1:31,],tijObjList[3][[1]][1:31,])
+identical(tijObjList[2][[1]][1:31,],tijObjList[4][[1]][1:31,])
+identical(tijObjList[2][[1]][1:31,],tijObjList[5][[1]][1:31,])
+identical(tijObjList[2][[1]][1:31,],tijObjList[6][[1]][1:31,])
+identical(tijObjList[3][[1]][1:31,],tijObjList[4][[1]][1:31,])
+identical(tijObjList[3][[1]][1:31,],tijObjList[5][[1]][1:31,])
+identical(tijObjList[3][[1]][1:31,],tijObjList[6][[1]][1:31,])
+identical(tijObjList[4][[1]][1:31,],tijObjList[5][[1]][1:31,])
+identical(tijObjList[4][[1]][1:31,],tijObjList[6][[1]][1:31,])
+identical(tijObjList[5][[1]][1:31,],tijObjList[6][[1]][1:31,])
